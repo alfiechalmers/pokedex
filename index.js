b355b86df101be0d53aca4b3640c1be73e95f279
Alfie <alfiechalmers@yahoo.com>
2021-04-14 21:32:55 +0100

    added enter key support for searching

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function searchPokedex() {
-    let pokemonInput = document.getElementById("pokemonInput").value;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput}`)
+    let pokemonInput = document.getElementById("pokemonInput").value.toLowerCase();
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput}`)
         .then(response => {
             return response.json();
         })
         .then(pokemon => {
-            document.getElementById("pokemonName").innerHTML = pokemon.species.name;
+            let lowerCaseName = pokemon.species.name;
+            document.getElementById("pokemonName").innerHTML = lowerCaseName.charAt(0).toUpperCase() + lowerCaseName.slice(1);
             document.getElementById("pokemonImage").src = pokemon.sprites.other["official-artwork"].front_default;
         })
 }
+const input = document.getElementById("pokemonInput");
+input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        searchPokedex();
+    }
+})
